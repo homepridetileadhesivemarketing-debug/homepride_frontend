86380058d135fc5806c1451ff102d259ef6a6124
homepridetileadhesivemarketing-debug <homepridetileadhesivemarketing@gmail.com>
2026-04-10 15:37:41 +0530

    first commit code by abhishek

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    // <div className="App">
+    // </div>
+    <>
+    {/* <Home></Home> */}
+    <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home(){
+    return(
+        <>
+        <p>hello home pride homepage</p>
+        </>
+    )
+}
+
+export default Home;
